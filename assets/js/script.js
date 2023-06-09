7818f682e001997a83d93530dcf2b2a6b051a0ca
Jose Albornoz <prueba@outlook.cl>
2023-06-09 02:57:01 -0400

    reparacion de borde en imagen sticker-logo

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,10 @@ document.addEventListener("click", (e) => {
     if (e.target.id == "sticker-logo") {
         const borderImg = document.getElementById("sticker-logo");
 
-        if (borderImg.style.border == "none") {
-            borderImg.style.border = "2px solid red";
+        if (borderImg.style.border == "2px solid red") {
+            borderImg.style.border = "none";
         } else {
-            borderImg.style.border = "";
+            borderImg.style.border = "2px solid red";
         }
     }
 
