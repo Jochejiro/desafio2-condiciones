60d7dc74bc2cd5ae20e8f37eb2d2d212721925aa
Jose Albornoz <prueba@outlook.cl>
2023-06-09 00:03:37 -0400

    optimizando codigo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,13 @@
-let border = false;
 
 document.addEventListener("click", (e) => {
     // Desafio - Parte 1
     if (e.target.id == "sticker-logo") {
         const borderImg = document.getElementById("sticker-logo");
 
-        if (!border) {
-            borderImg.style.border = "2px solid red";
-            border = true;
-        } else {
+        if (borderImg.style.border == "2px solid red") {
             borderImg.style.border = "";
-            border = false;
+        } else {
+            borderImg.style.border = "2px solid red";
         }
     }
 
